8e68a46ba36d7cd1563a3153c5665da067098dc5
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-04-21 07:13:36 +0530

    Added solution - LeetHub

---
+++ 700221-lca-in-binary-tree-GFG/700221-lca-in-binary-tree.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    Node lca(Node root, int p, int q) {
+        if(root == null ) return null;
+        if(root.data == p || root.data == q )  return root;
+        boolean pLiesInLeft  = exits(root.left,p);
+        boolean qLiesInLeft  = exits(root.left,q);
+        if(pLiesInLeft && qLiesInLeft)   return lca(root.left, p ,q);
+        else if(!pLiesInLeft && !qLiesInLeft)   return lca(root.right, p ,q);
+        else return  root;
+        
+    }
+    
+    private boolean  exits(Node root, int val) {
+        if(root ==  null)  return false;
+        if(root.data ==  val)  return true;
+        return exits(root.left, val) ||  exits(root.right, val);
+        
+    }
+}
