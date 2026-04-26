c77b3c81ef97e8086f9e85d823b528bf19b57bf4
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-04-26 15:08:31 +0530

    Added solution - LeetHub

--- 700135-inorder-traversal-GFG/700135-inorder-traversal.java
+++ 700135-inorder-traversal-GFG/700135-inorder-traversal.java
@@ -1,15 +1,45 @@
 class Solution {
-    public void dfs(Node root, ArrayList<Integer> ans) {
-        if(root == null) return;
-        dfs(root.left,ans);
-        ans.add(root.data);
-        dfs(root.right,ans);
+    
+     public ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Node> st  = new Stack<>();
+        Node curr  = root;
+        while(st.size()>0 || curr!= null) {
+            if(curr !=  null) {
+                if(curr.left != null) {
+                st.push(curr);
+                curr =  curr.left;
+                }
+                else{
+                    ans.add(curr.data);
+                    curr =  curr.right;
+                }
+            }
+            
+            else{  //  curr  ==  null 
+                 Node top = st.pop();
+                 ans.add(top.data);
+                 curr =  top.right;
+            }
+        }
+        return ans; 
+    }
+    
+    
+    
+    
+    
+    // public void dfs(Node root, ArrayList<Integer> ans) {
+    //     if(root == null) return;
+    //     dfs(root.left,ans);
+    //     ans.add(root.data);
+    //     dfs(root.right,ans);
 
         
-    }
-    public ArrayList<Integer> inOrder(Node root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        dfs(root,ans);
-        return ans;
-    }
+    // }
+    // public ArrayList<Integer> inOrder(Node root) {
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     dfs(root,ans);
+    //     return ans;
+    // }
 }
