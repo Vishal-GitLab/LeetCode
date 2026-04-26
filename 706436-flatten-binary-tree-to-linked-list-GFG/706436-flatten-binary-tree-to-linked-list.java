c66fe8ea61f367c0ead468de68b6fed122334b86
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-04-26 08:22:03 +0530

    Added solution - LeetHub

--- 706436-flatten-binary-tree-to-linked-list-GFG/706436-flatten-binary-tree-to-linked-list.java
+++ 706436-flatten-binary-tree-to-linked-list-GFG/706436-flatten-binary-tree-to-linked-list.java
@@ -2,24 +2,36 @@
 
 class Solution {
      public static void flatten(Node root) {
-            ArrayList<Node>  arr =  new ArrayList<>();
-            dfs(root, arr);
-            for (int i = 0; i < arr.size()-1; i++) {
-                Node a  = arr.get(i);
-                Node b = arr.get(i+1);
-                a.right = b;
-                a.left = null;
-            }
-            Node last = arr.get(arr.size()-1);
-            last.left = null;
-            last.right = null;
-        }
+         if(root==null) return;
+         Node lst  = root.left;
+         Node rst  = root.right;
+         root.left = null;
+         flatten(lst);   // magic
+         flatten(rst);
+         root.right = lst;
+         Node last  = root;
+         while(last.right != null) last = last.right;
+         last.right= rst;
+         
+         
+    //         ArrayList<Node>  arr =  new ArrayList<>();
+    //         dfs(root, arr);
+    //         for (int i = 0; i < arr.size()-1; i++) {
+    //             Node a  = arr.get(i);
+    //             Node b = arr.get(i+1);
+    //             a.right = b;
+    //             a.left = null;
+    //         }
+    //         Node last = arr.get(arr.size()-1);
+    //         last.left = null;
+    //         last.right = null;
+    //     }
 
-    private static void dfs(Node root, ArrayList<Node> arr) {
+    // private static void dfs(Node root, ArrayList<Node> arr) {
 
-        if(root == null) return;
-        arr.add(root);
-        dfs(root.left,arr);
-        dfs(root.right,arr);
-    }
+    //     if(root == null) return;
+    //     arr.add(root);
+    //     dfs(root.left,arr);
+    //     dfs(root.right,arr);
+     }
 }
