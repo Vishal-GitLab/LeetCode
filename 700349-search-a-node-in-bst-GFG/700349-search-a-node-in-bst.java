946bd69011c6dc9d307e9335d5b2712202fa25d5
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-04-30 12:02:44 +0530

    Added solution - LeetHub

---
+++ 700349-search-a-node-in-bst-GFG/700349-search-a-node-in-bst.java
@@ -0,0 +1,12 @@
+
+
+class Solution {
+    public boolean search(Node root, int x) {
+      
+      if(root == null) return false;
+      if(root.data ==  x) return true;
+      else if(root.data > x) return search( root.left,x);
+      else   return search( root.right,x);
+     
+    }
+}
