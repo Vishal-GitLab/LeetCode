e59966eff869f063540633cddc381d0475c90d6c
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-05-01 10:23:32 +0530

    Added solution - LeetHub

--- 700216-bst-insert-GFG/700216-bst-insert.java
+++ 700216-bst-insert-GFG/700216-bst-insert.java
@@ -1,23 +1,33 @@
 class Solution {
     
     // Function to insert a node in a BST
-    void attach(Node root, int key) {
-        if(root==null)  return;
-        if(root.data == key )  return;
-        if(root.data < key ) {
-            if(root.right == null)  root.right =  new Node(key);
-            else attach(root.right, key);
-        }
-        else {  // root.data > data
-            if(root.left == null)  root.left =  new Node(key);
-            else attach(root.left, key);
+    // void attach(Node root, int key) {
+    //     if(root==null)  return;
+    //     if(root.data == key )  return;
+    //     if(root.data < key ) {
+    //         if(root.right == null)  root.right =  new Node(key);
+    //         else attach(root.right, key);
+    //     }
+    //     else {  // root.data > data
+    //         if(root.left == null)  root.left =  new Node(key);
+    //         else attach(root.left, key);
             
-        }
-    }
+    //     }
+    // }
     
-    public Node insert(Node root, int key) {
-           attach(root, key);
-           return root;
+    // public Node insert(Node root, int key) {
+    //       attach(root, key);
+    //       return root;
+    // }
+    
+    
+    
+     public Node insert(Node root, int key) {
+           if( root == null)  return   new Node(key);
+           if(root.data ==  key ) return root;
+           else  if(root.data < key) root.right =  insert(root.right,  key);
+           else  root.left = insert(root.left,  key);
+            return root;
     }
 
 }
