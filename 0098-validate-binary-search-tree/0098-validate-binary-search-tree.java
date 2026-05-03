a1b0938220b09d80a0f88edc8788809cd4bd6066
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-05-03 09:08:52 +0530

    Time: 1 ms (17.29%), Space: 46.6 MB (6.96%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,18 +1,48 @@
 
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-         ArrayList<Integer>  arr  = new ArrayList<>();
-         inorder(root,arr);
-         for( int i = 1; i < arr.size(); i++) {
-             if(arr.get(i) <= arr.get(i-1)) return false;
-         }
-         return true;
-    }
-    public void inorder(TreeNode root, ArrayList<Integer>  arr) {
-          if(root == null) return;
-          inorder(root.left, arr);
-          arr.add(root.val);
-          inorder(root.right, arr);
- 
+
+ class Pair{
+    long max;
+    long min;
+     Pair(long max, long min) {
+        this.max =  max;
+        this.min = min;
     }
 }
+
+class Solution {
+    static boolean flag;
+    boolean isValidBST(TreeNode root){
+        flag = true;
+        maxMin(root);
+        return flag;
+    }
+  Pair maxMin(TreeNode root) {
+      if(root == null) return new Pair(Long.MIN_VALUE,Long.MAX_VALUE);
+      Pair lst  = maxMin(root.left);
+        Pair rst  = maxMin(root.right);
+        //ab left subtree and right subtree and root ka max and min nikalenge
+        long data = (long)(root.val);
+        long max  = Math.max(data,Math.max(lst.max,rst.max));
+          long min  = Math.min(data,Math.min(lst.min,rst.min));
+          if(lst.max>= data || rst.min <= data)
+          flag =false;
+          return new Pair(max,min);
+  }
+
+} 
+//     public boolean isValidBST(TreeNode root) {
+//          ArrayList<Integer>  arr  = new ArrayList<>();
+//          inorder(root,arr);
+//          for( int i = 1; i < arr.size(); i++) {
+//              if(arr.get(i) <= arr.get(i-1)) return false;
+//          }
+//          return true;
+//     }
+//     public void inorder(TreeNode root, ArrayList<Integer>  arr) {
+//           if(root == null) return;
+//           inorder(root.left, arr);
+//           arr.add(root.val);
+//           inorder(root.right, arr);
+ 
+//     }
+// }
