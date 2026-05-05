6863da63c579075c0819865ef095e35ac1a4f965
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-05-05 07:15:04 +0530

    Added solution - LeetHub

--- 700149-check-for-bst-GFG/700149-check-for-bst.java
+++ 700149-check-for-bst-GFG/700149-check-for-bst.java
@@ -1,33 +1,68 @@
 
- class Pair{
-    int max;
-    int min;
-     Pair(int max, int min) {
-        this.max =  max;
-        this.min = min;
-    }
+class Solution{
+       boolean isBST(Node root){
+          Node curr = root;
+          int prev = 0;
+         while(curr != null) {
+            if(curr.left != null){
+               // find pred and do work 
+               Node pred = curr.left;
+               while(pred.right!=null && pred.right != curr)
+               pred = pred.right;
+               if(pred.right == null) {  // link
+                   pred.right = curr;
+                   curr  = curr.left;
+              }
+               else{ // pred.right = curr // unlinked & print
+                   pred.right = null;
+                   if(curr.data <= prev)  return false;
+                   prev =  curr.data;
+                   curr =  curr.right;
+            }
+        }
+        else {
+             if(curr.data <= prev)  return false;
+                   prev =  curr.data;
+                   curr =  curr.right;
+           }
+        }   
+        return true;
+     
+  }
 }
 
-class Solution {
-    static boolean flag;
-    boolean isBST(Node root){
-        flag = true;
-        maxMin(root);
-        return flag;
-    }
-  Pair maxMin(Node root) {
-      if(root == null) return new Pair(Integer.MIN_VALUE,Integer.MAX_VALUE);
-      Pair lst  = maxMin(root.left);
-        Pair rst  = maxMin(root.right);
-        //ab left subtree and right subtree and root ka max and min nikalenge
-        int max  = Math.max(root.data,Math.max(lst.max,rst.max));
-          int min  = Math.min(root.data,Math.min(lst.min,rst.min));
-          if(lst.max>= root.data || rst.min <= root.data)
-          flag =false;
-          return new Pair(max,min);
-  }
+//  class Pair{
+//     int max;
+//     int min;
+//      Pair(int max, int min) {
+//         this.max =  max;
+//         this.min = min;
+//     }
+// }
 
-}    
+// class Solution {
+//     static boolean flag;
+//     boolean isBST(Node root){
+//         flag = true;
+//         maxMin(root);
+//         return flag;
+//     }
+//   Pair maxMin(Node root) {
+//       if(root == null) return new Pair(Integer.MIN_VALUE,Integer.MAX_VALUE);
+//       Pair lst  = maxMin(root.left);
+//         Pair rst  = maxMin(root.right);
+//         //ab left subtree and right subtree and root ka max and min nikalenge
+//         int max  = Math.max(root.data,Math.max(lst.max,rst.max));
+//           int min  = Math.min(root.data,Math.min(lst.min,rst.min));
+//           if(lst.max>= root.data || rst.min <= root.data)
+//           flag =false;
+//           return new Pair(max,min);
+//   }
+
+// }    
+
+
+
 
     //     boolean isBST(Node root) {
     //      ArrayList<Integer>  arr  = new ArrayList<>();
