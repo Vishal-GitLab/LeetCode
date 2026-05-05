e2c7d4040d2b8045c9c0d15de3792024a512825b
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-05-05 07:27:41 +0530

    Time: 1 ms (17.45%), Space: 45.4 MB (21.76%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,35 +1,74 @@
-
-
- class Pair{
-    long max;
-    long min;
-     Pair(long max, long min) {
-        this.max =  max;
-        this.min = min;
-    }
+class Solution {
+    boolean isValidBST(TreeNode root){
+          TreeNode curr = root;
+          long prev = Long.MIN_VALUE;
+         while(curr != null) {
+            if(curr.left != null){
+               // find pred and do work 
+               TreeNode pred = curr.left;
+               while(pred.right!=null && pred.right != curr)
+               pred = pred.right;
+               if(pred.right == null) {  // link
+                   pred.right = curr;
+                   curr  = curr.left;
+              }
+               else{ // pred.right = curr // unlinked & print
+                   pred.right = null;
+                   if(curr.val <= prev)  return false;
+                   prev =  curr.val;
+                   curr =  curr.right;
+            }
+        }
+        else {
+             if(curr.val <= prev)  return false;
+                   prev =  curr.val;
+                   curr =  curr.right;
+           }
+        }   
+        return true;
+   }
 }
 
-class Solution {
-    static boolean flag;
-    boolean isValidBST(TreeNode root){
-        flag = true;
-        maxMin(root);
-        return flag;
-    }
-  Pair maxMin(TreeNode root) {
-      if(root == null) return new Pair(Long.MIN_VALUE,Long.MAX_VALUE);
-      Pair lst  = maxMin(root.left);
-        Pair rst  = maxMin(root.right);
-        //ab left subtree and right subtree and root ka max and min nikalenge
-        long data = (long)(root.val);
-        long max  = Math.max(data,Math.max(lst.max,rst.max));
-          long min  = Math.min(data,Math.min(lst.min,rst.min));
-          if(lst.max>= data || rst.min <= data)
-          flag =false;
-          return new Pair(max,min);
-  }
 
-} 
+
+
+//  class Pair{
+//     long max;
+//     long min;
+//      Pair(long max, long min) {
+//         this.max =  max;
+//         this.min = min;
+//     }
+// }
+
+// class Solution {
+//     static boolean flag;
+//     boolean isValidBST(TreeNode root){
+//         flag = true;
+//         maxMin(root);
+//         return flag;
+//     }
+//   Pair maxMin(TreeNode root) {
+//       if(root == null) return new Pair(Long.MIN_VALUE,Long.MAX_VALUE);
+//       Pair lst  = maxMin(root.left);
+//         Pair rst  = maxMin(root.right);
+//         //ab left subtree and right subtree and root ka max and min nikalenge
+//         long data = (long)(root.val);
+//         long max  = Math.max(data,Math.max(lst.max,rst.max));
+//           long min  = Math.min(data,Math.min(lst.min,rst.min));
+//           if(lst.max>= data || rst.min <= data)
+//           flag =false;
+//           return new Pair(max,min);
+//   }
+
+// } 
+
+
+
+
+
+
+
 //     public boolean isValidBST(TreeNode root) {
 //          ArrayList<Integer>  arr  = new ArrayList<>();
 //          inorder(root,arr);
