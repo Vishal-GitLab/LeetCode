ca4ab65b50acf441014e9acc253afe4d5c09147c
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-08-31 11:59:17 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.09%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+ 
+            ListNode current = head;
+            int result  = 0;
+
+            while(current!=null){
+                 result  = result * 2 + current.val;
+                   current = current.next;
+            }
+         
+             return result;
+
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
