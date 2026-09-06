b81d10870243dd854f81283af218556f8c6e595d
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-06 08:57:30 +0530

    Time: 34 ms (61.34%), Space: 99.7 MB (76.4%) - LeetHub - Time: O(1), Space: O(n)

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -24,12 +24,8 @@ class MinStack {
         return minSt.peek();
     }
 }
+ 
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
