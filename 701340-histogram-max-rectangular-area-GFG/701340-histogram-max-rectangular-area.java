710068242a7f4d423bfe2b74d73513ab3089a74f
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-11 15:28:29 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701340-histogram-max-rectangular-area-GFG/701340-histogram-max-rectangular-area.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static int getMaxArea(int arr[]) {
+        int n = arr.length;
+               int[] nse =  new int[n];
+               nse[n-1] = n; // or -1
+               Stack<Integer> st  = new Stack<>();
+               st.push(n-1);
+               for(int i = n-2; i>=0; i--) {
+                   while(st.size()>0 && arr[st.peek()] >= arr[i]) st.pop();
+                   if(st.size()==0)  nse[i] = n;
+                   else nse[i] = st.peek();
+                   st.push(i);
+               }
+                  while(st.size()>  0) st.pop();
+                  int[] pse = new int[n];
+                  pse[0] = -1;
+                  st.push(0);
+                 for(int i = 1; i < n; i++) {
+                   while(st.size()>0 && arr[st.peek()] >= arr[i]) st.pop();
+                   if(st.size()==0)  pse[i] = -1;
+                   else pse[i] = st.peek();
+                   st.push(i);
+               }
+               int maxArea = 0;
+               for(int i = 0; i<n; i++) {
+                   int area = arr[i]*(nse[i]-pse[i]-1);
+                   maxArea = Math.max(maxArea,area);
+               }
+               return maxArea;
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
