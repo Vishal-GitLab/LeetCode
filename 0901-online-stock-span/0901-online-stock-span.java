df28610e773ccd5c002cfb1f34e0442ed413bcee
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-11 17:01:48 +0530

    Time: 32 ms (47.11%), Space: 55.7 MB (13.27%) - LeetHub - Time: O(1) amortized, Space: O(n)

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,28 @@
+class StockSpanner {
+         Stack<int[]> stack; 
+
+
+    public StockSpanner() {
+       stack = new Stack();
+    }
+    
+    public int next(int price) {
+        int ans = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price) {
+            ans +=  stack.pop()[1];
+        }
+        stack.push(new int[] {price,ans});  
+         
+        return ans;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
