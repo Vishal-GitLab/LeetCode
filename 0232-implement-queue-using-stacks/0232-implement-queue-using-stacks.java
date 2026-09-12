44d92c905528a3a8aa7a68362bce11d1148bba4c
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-12 22:35:35 +0530

    Time: 1 ms (28%), Space: 42.7 MB (69.32%) - LeetHub - Time: O(n), Space: O(n)

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -2,36 +2,56 @@ class MyQueue {
      Stack<Integer> st =  new Stack<>();
      Stack<Integer> helper =  new Stack<>();
     public MyQueue() {
-        
+
     }
     
-    public void push(int x) {
-        st.push(x);
+    public void push(int x) {  // 0(n)
+          // push at bottom of st
+          while(st.size()>0){
+             helper.push(st.pop());
+          }
+          st.push(x);
+          while(helper.size()>0) {
+            st.push(helper.pop());
+          }
     }
     
     public int pop() {
-        // st ka bottom remove karne ke liye;;
-        while(st.size() > 1) {
-            helper.push(st.pop());
-        }
-        // st se delete krayenge usko kahi store krayenge;;
-        int front = st.pop();
-        while(helper.size() > 0) {
-            st.push(helper.pop());
-        }
-        return front;
+       return st.pop();
     }
     
     public int peek() {
-        while(st.size() > 1) {
-            helper.push(st.pop());
-        }
-        int front  = st.peek();
-        while(helper.size() > 0) {
-            st.push(helper.pop());
-        }
-         return front;
-    }
+      return st.peek();
+    }   
+    // }
+    
+    // public void push(int x) {
+    //     st.push(x);
+    // }
+    
+    // public int pop() {
+    //     // st ka bottom remove karne ke liye;;
+    //     while(st.size() > 1) {
+    //         helper.push(st.pop());
+    //     }
+    //     // st se delete krayenge usko kahi store krayenge;;
+    //     int front = st.pop();
+    //     while(helper.size() > 0) {
+    //         st.push(helper.pop());
+    //     }
+    //     return front;
+    // }
+    
+    // public int peek() {
+    //     while(st.size() > 1) {
+    //         helper.push(st.pop());
+    //     }
+    //     int front  = st.peek();
+    //     while(helper.size() > 0) {
+    //         st.push(helper.pop());
+    //     }
+    //      return front;
+    // }
     
     public boolean empty() {
         return (st.size()==0);
@@ -46,3 +66,7 @@ class MyQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
