84eb21ad25708c5c67cd1fb6456754b34a1063c7
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-13 22:41:24 +0530

    Time: 5 ms (30.55%), Space: 46.5 MB (70.9%) - LeetHub - Time: O(1), Space: O(k)

---
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -0,0 +1,126 @@
+class MyCircularQueue {
+
+    int[] queue;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+    public MyCircularQueue(int k) {
+        queue = new int[k];
+        capacity = k;
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    public boolean enQueue(int value) {
+
+        if (isFull()) {
+            return false;
+        }
+
+        rear = (rear + 1) % capacity;
+        queue[rear] = value;
+        size++;
+
+        return true;
+    }
+
+    public boolean deQueue() {
+
+        if (isEmpty()) {
+            return false;
+        }
+
+        front = (front + 1) % capacity;
+        size--;
+
+        return true;
+    }
+
+    public int Front() {
+
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return queue[front];
+    }
+
+    public int Rear() {
+
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return queue[rear];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MyCircularQueue {
+
+//     public MyCircularQueue(int k) {
+        
+//     }
+    
+//     public boolean enQueue(int value) {
+        
+//     }
+    
+//     public boolean deQueue() {
+        
+//     }
+    
+//     public int Front() {
+        
+//     }
+    
+//     public int Rear() {
+        
+//     }
+    
+//     public boolean isEmpty() {
+        
+//     }
+    
+//     public boolean isFull() {
+        
+//     }
+// }
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
