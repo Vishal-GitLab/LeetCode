610d4b4c248f37524f4ac529963d450ca0b5b2a5
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-23 17:48:55 +0530

    Time: 35 ms (75.73%), Space: 48 MB (23.6%) - LeetHub - Time: O(n), Space: O(min(n, m))

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+         HashSet<Character> set = new HashSet<>();
+       int  i = 0, j = 0, maxLen = 0;
+       while (j<s.length()) {
+           char ch = s.charAt(j);
+           if (!set.contains(ch)) {
+               set.add(ch);
+               j++;
+           }
+           else {
+               int  len = j-i;
+               maxLen =  Math.max(maxLen,len);
+               while (s.charAt(i) != s.charAt(j)) {
+                   set.remove(s.charAt(i));
+                   i++;
+               }
+               i++;
+               j++;
+           }
+       }
+       int  len = j-i;
+       maxLen =  Math.max(maxLen,len);
+         return maxLen;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
