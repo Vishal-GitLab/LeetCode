8d0fca445cf36220e45e8adadf347ddb1f1e4be5
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-24 19:23:42 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 702131-burning-tree-GFG/702131-burning-tree.java
@@ -0,0 +1,59 @@
+ class Pair{
+     Node node;
+     int dist;
+     Pair(Node node, int dist){
+         this.node = node;
+         this.dist = dist;
+     }
+ }
+class Solution {
+    static Node start;
+       static HashMap<Node, Node> parent;
+
+       public static int minTime(Node root, int target) {
+           start = null;
+           parent = new HashMap<>();
+           dfs(root, target);
+           Queue<Pair> q = new LinkedList<>();
+           q.add(new Pair(start, 0));
+           HashSet<Node> burned = new HashSet<>();
+           burned.add(start);
+           int time = 0;
+           while (q.size() > 0) {
+               Pair front = q.remove();
+               int dist = front.dist;
+               ;
+               time = Math.max(time, dist);
+               Node node = front.node;
+               ;
+               if (node.left != null && !burned.contains(node.left)) {
+                   q.add(new Pair(node.left, dist + 1));
+                   burned.add(node.left);
+               }
+               if (node.right != null && !burned.contains(node.right)) {
+                   q.add(new Pair(node.right, dist + 1));
+                   burned.add(node.right);
+               }
+               if (parent.containsKey(node) && !burned.contains(parent.get(node))) {
+                   q.add(new Pair(parent.get(node), dist + 1));
+                   burned.add(parent.get(node));
+               }
+           }
+           return time;
+       }
+
+
+       private static void dfs(Node root, int target) {
+           if (root == null) return;;
+           if (root.data  == target) start = root;
+           if (root.left != null) parent.put(root.left,root);
+           if (root.right != null) parent.put(root.right,root);
+           dfs(root.left,target);
+           dfs(root.right,target);
+       }
+   
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
