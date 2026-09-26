b445852d17a7409666079a8beb45cc48adc9121e
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-26 17:37:15 +0530

    Time: 0 ms (100%), Space: 42.7 MB (11.79%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingWeight(int n) {
+       int count = 0;
+       while(n != 0) {
+        n = n & (n-1);
+        count ++;
+       }
+       return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
