00e25c5d8fc725beba595322846682351eed5b1a
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2026-09-26 17:22:39 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 701154-check-kth-bit-GFG/701154-check-kth-bit.java
@@ -0,0 +1,9 @@
+class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        return ((n>>k)%2==1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
